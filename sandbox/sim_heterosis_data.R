e57408b4d7d1ade914ee0f54e8733c841a87c4a0
Jarad Niemi <niemi@iastate.edu>
2015-03-07 08:08:40 -0600

    simulate heterosis data

---
+++ sandbox/sim_heterosis_data.R
@@ -0,0 +1,45 @@
+#' Simulate heterosis data
+#' 
+#' Simulates heterosis data from either hyperparameters (and thus simulating gene-specific parameters)
+#' or gene-specific parameters.
+#' 
+#' @param G integer. The number of genes to simulate.
+#' @param nv integer (or integer vector). indicating the number of samples per genotype
+#' @param parameters. a list containing either hyperparameters or (gene-specific) parameters
+#' @param distributions. a list containing the distribution to use for the gene-specific parameters
+#' 
+#' @return list containing the hyperparameters, gene-specific parameters, and data
+#' 
+
+sim_heterosis_data = function(G=10, nv=4, parameters=NULL, distributions=NULL) {
+  require(plyr)
+  
+  if (length(nv==1)) nv = rep(nv,3)
+  
+  if (is.null(parameters)) 
+    hyperparameters = data.frame(parameter      = c("phi","alpha","delta","psi"),
+                                 location = c(4.6,0,0,-2),
+                                 scale    = c(1.8,.1,.01,.1))
+  
+  # Simulate gene-specific parameters
+  if (is.null(distributions)) 
+    parameters = rdply(G, {
+      with(hyperparameters, 
+           data.frame(phi   = rnorm   (1, location[parameter == "phi"  ], scale[parameter == "phi"  ]),
+                      alpha = rlaplace(1, location[parameter == "alpha"], scale[parameter == "alpha"]),
+                      delta = rlaplace(1, location[parameter == "delta"], scale[parameter == "delta"]),
+                      psi   = rnorm   (1, location[parameter == "psi"  ], scale[parameter == "psi"  ])))
+    }, .id="gene")
+  
+  data = ddply(data.frame(gene=1:G), .(gene), function(x) {
+    g = x$gene
+    mutate(data.frame(parent = rep(1:3, times = nv)),
+           sample = 1:sum(nv),
+           eta = with(parameters, c(phi[g]+alpha[g], phi[g]-alpha[g], phi[g]+delta[g])),
+           y = rnbinom(length(eta), size = 1/exp(parameters$psi[g]), mu = exp(eta)))
+  })
+  
+  return(list(data            = data, 
+              parameters      = parameters,
+              hyperparameters = hyperparameters))
+}
