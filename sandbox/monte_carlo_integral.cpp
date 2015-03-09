1f53dd5106a346c17833100e18bf1b0cfbcc9f0d
Jarad Niemi <niemi@iastate.edu>
2015-03-09 12:20:18 -0500

    implement logsumexp

--- sandbox/monte_carlo_integral.cpp
+++ sandbox/monte_carlo_integral.cpp
@@ -7,6 +7,20 @@ using namespace Rcpp;
 
 // For more on using Rcpp click the Help button on the editor toolbar
 
+double logsumexp(double x[], int n) {
+  double max_val = x[0], sum = 0.0;
+  int i;
+  
+  for (i = 1; i< n; i++)
+    if (x[i] > max_val) 
+      max_val = x[i];
+      
+  for (i=0; i<n; i++)
+    sum += exp(x[i] - max_val);
+    
+  return log(sum) + max_val;
+}
+
 // [[Rcpp::export]]
 double monte_carlo_integral(IntegerMatrix count, 
                             IntegerVector variety, 
@@ -33,7 +47,7 @@ double monte_carlo_integral(IntegerMatrix count,
          delta_scale    = hyperparameters[6],
          psi_scale      = hyperparameters[7];
          
-  double integral = 0, partial_integral, log_mass, phi, alpha, delta, psi, mu[3], gene_avg;
+  double integral = 0, partial_integral, log_mass[n_sims], phi, alpha, delta, psi, mu[3], gene_avg;
   
   for (int g=0; g<G; g++) {
     partial_integral = 0;
@@ -43,28 +57,25 @@ double monte_carlo_integral(IntegerMatrix count,
     gene_avg /= n;
     
     for (int s=0; s<n_sims; s++) {
-      //phi   = rnorm(1, phi_location,   phi_scale)[0]; 
-      phi   = rnorm(1, log(gene_avg),   .1)[0]; 
+      phi   = rnorm(1, phi_location,   phi_scale)[0]; 
+      //phi   = rnorm(1, log(gene_avg),   .1)[0]; 
       alpha = rnorm(1, alpha_location, alpha_scale)[0]; // change to laplace
       delta = rnorm(1, delta_location, delta_scale)[0]; // change to laplace
-      psi   = rnorm(1, psi_location+2,   psi_scale)[0];
+      psi   = rnorm(1, psi_location,   psi_scale)[0];
       
       mu[0] = phi+alpha; // make sure these agree with definition
       mu[1] = phi-alpha;
       mu[2] = phi+delta;
       
-      log_mass = 0;
+      log_mass[s] = 0;
       for (int i=0; i<n; i++) {
-        log_mass += dnbinom_mu(count(g,i), 1/exp(psi), exp(mu[variety[i]]), 1);
-        Rprintf("%i %i %i\n %f %f %f %f\n %f %f %f\n %i %f\n", 
-                g, s, i, phi, alpha, delta, psi, mu[0], mu[1], mu[2], count(g,i),dnbinom_mu(count(g,i), 1/exp(psi), exp(mu[variety[i]]), 1));
+        log_mass[s] += dnbinom_mu(count(g,i), 1/exp(psi), exp(mu[variety[i]]), 1);
+        //Rprintf("%i %i %i\n %f %f %f %f\n %f %f %f\n %i %f\n", 
+        //        g, s, i, phi, alpha, delta, psi, mu[0], mu[1], mu[2], count(g,i),dnbinom_mu(count(g,i), 1/exp(psi), exp(mu[variety[i]]), 1));
         //Rprintf("  %i %i %i %f\n", g, s, i, log_mass);
       }
-      partial_integral += exp(log_mass);
-      Rprintf("   %f\n", partial_integral);
     }
-    
-    integral += log(partial_integral);
+    integral += logsumexp(log_mass, n_sims);
   }     
          
   return integral;
