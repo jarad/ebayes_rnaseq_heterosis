3df1e2007d941324b01f8755e04a082046fc9539
Jarad Niemi <niemi@iastate.edu>
2015-03-11 17:02:05 -0500

    subtle

--- sandbox/find_map_estimates.R
+++ sandbox/find_map_estimates.R
@@ -1,5 +1,8 @@
 library(Rcpp)
 library(reshape2)
+library(plyr)
+library(ggplot2)
+library(MASS)
 
 source("sim_heterosis_data.R")
 source("Laplace.R")
@@ -7,7 +10,8 @@ source("Laplace.R")
 sourceCpp("monte_carlo_integral.cpp", verbose=TRUE)
 
 # Get data
-all = sim_heterosis_data(G=100)
+set.seed(1)
+all = sim_heterosis_data(G=2)
 R = rbind(c(.5, .5, 0),
           c(.5,-.5, 0),
           c(.5, .5,-1))
@@ -15,7 +19,6 @@ X = with(all$data[all$data$gene==1,],
          cbind(parent==1, parent==2, parent==3)) %*% solve(R)
 
 # Observed Fisher information
-library(MASS)
 
 data_summary = ddply(mutate(all$data, gene=as.factor(gene), parent=as.factor(parent)),
           .(gene), 
@@ -23,7 +26,7 @@ data_summary = ddply(mutate(all$data, gene=as.factor(gene), parent=as.factor(par
             m = glm.nb(formula = y~0+X, data=x)
             data.frame(
               parameter = c('phi', 'alpha', 'delta', 'psi'),
-              estimate = c(coef(m), log(summary(m)$theta)),
+              estimate = c(coef(m), -log(summary(m)$theta)),
               sd = sqrt(c(diag(vcov(m, summary(m)$theta)),NA)))
           })
 
@@ -37,10 +40,10 @@ hyperparameters = melt(all$hyperparameters)
 # sourceCpp("monte_carlo_integral.cpp", verbose=TRUE)
 
 log_like = function(pi) {
-  -monte_carlo_integral(t(as.matrix(count[,-1])), variety-1, pi[1:4], exp(pi[5:8]), 
+  monte_carlo_integral(t(as.matrix(count[,-1])), variety-1, pi[1:4], exp(pi[5:8]), 
                         with(data_summary, estimate[parameter=='phi']),
                         with(data_summary,       sd[parameter=='phi']),
-                        1000)
+                        10)
 }
 
 r = ddply(data.frame(n_sims = 10^seq(1,4)), .(n_sims), function(x) {
@@ -49,10 +52,12 @@ r = ddply(data.frame(n_sims = 10^seq(1,4)), .(n_sims), function(x) {
                                              with(data_summary,       sd[parameter=='phi']),
                                              x$n_sims)))
 })
-ggplot(r, aes(x=n_sims,y=integral))+geom_point() + scale_x_log10()
+
+r$ei = exp(r$integral-max(r$integral))
+ggplot(r, aes(x=n_sims,y=ei))+geom_point() + scale_x_log10()
 
 
-o = optim(rep(0,8), log_like)
+o = optim(rep(0,8), log_like, control = list(fnscale=-1))
 cbind(melt(all$hyperparameters, value.var="truth"), estimate = c(o$par[1:4], exp(o$par[5:8])))
 
 
