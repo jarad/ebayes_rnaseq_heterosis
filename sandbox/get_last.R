dd54c9f0bed5ebee556768430c4d7b68fbafd989
Jarad Niemi <niemi@iastate.edu>
2015-04-16 16:06:06 -0500

    rm get_last.R

--- sandbox/get_last.R
+++
@@ -1,6 +0,0 @@
-get_last = function(stanfit) {
-  # Takes a stanfit object and returns the last samples in a format
-  # that can be used by the 'init' argument in the 'sampling' function
-  
-  
-}
