0e0d8d7a91295a562771b1c89a07ce4563204c60
Jarad Niemi <niemi@iastate.edu>
2015-05-15 10:38:26 -0500

    removed extraneous script

--- supp/script.R
+++ supp/script.R
@@ -21,51 +21,10 @@ design = cbind(1,
                rep(c(0, 0,1), each=4))
 
 # GLM fit
-dge = d %>% 
+fit = d %>% 
   DGEList() %>%
   calcNormFactors %>%
   estimateCommonDisp(design) %>%
   estimateGLMTagwiseDisp(design) %>%
   glmFit(design)
 
-
-
-
-# long format
-m = melt(d, 
-         id.var = "GeneID",
-         variable.name="sample", 
-         value.name="count")
-m$variety = factor(gsub("_[1-4]", 
-                        "", 
-                        as.character(m$sample)),
-                   levels=c("B73","Mo17","B73xMo17"))
-
-ind = ddply(m, .(GeneID), function(x) {
-  mo = glm.nb(count+1~variety, data=x)
-  as.data.frame(coef(mo))
-}, .inform=TRUE)
-
-r = glm(count+1~variety,data=test, family="poisson")
-
-tmp = 
-m %>% 
-  group_by(GeneID, variety) %>%
-  summarize(mean=mean(count)) %>%
-  dcast(GeneID~variety, value.var="mean")
-
-zeros = 
-tmp %>% 
-  rowwise() %>%
-  summarize(any = B73==0 | Mo17 == 0 | B73xMo17 == 0)
-mean(zeros$any)
-sum(zeros$any)
-
-
-tmp[zeros$any,] 
-  
-
-
-rm = rowMeans(d[,-1])
-
-mean(rowM)
