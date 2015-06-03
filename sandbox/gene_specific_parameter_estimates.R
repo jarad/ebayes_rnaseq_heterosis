ef42f7e61773d4d6ee8db4f28a516614599ad7d9
Jarad Niemi <niemi@iastate.edu>
2015-06-03 15:02:41 -0500

    return estimates

--- sandbox/gene_specific_parameter_estimates.R
+++ sandbox/gene_specific_parameter_estimates.R
@@ -8,7 +8,7 @@ source("sim_heterosis_data.R")
 
 # 
 G = 250
-d = sim_heterosis_data(G, verbose=1)
+d = sim_heterosis_data(G, nv=4, verbose=1)
 p = d$parameters # Get gene specific parameter draws
 
 # phi, alpha, delta parameterization
@@ -17,6 +17,7 @@ design = cbind(1,
                c(1,-1,0)[variety],
                variety == 3)
 
+tmp = 
 rdply(10, {
   fit = sim_heterosis_data(G, parameters = p)$data[,c("gene","sample","count")]  %>% 
     dcast(formula = gene~sample, value.var='count') %>% # Assumes columns are in variety order
@@ -28,11 +29,11 @@ rdply(10, {
     estimateGLMTagwiseDisp(design) %>%
     glmFit(design)
   
-  hat = data.frame(gene = 1:length(fit$dispersion),
-                   phi   = fit$coefficients[,1] + mean(fit$offset[1,]),
-                   alpha = fit$coefficients[,2],
-                   delta = fit$coefficients[,3],
-                   psi   = log(fit$dispersion))
-})
+  data.frame(gene = 1:length(fit$dispersion),
+             phi   = fit$coefficients[,1] + mean(fit$offset[1,]),
+             alpha = fit$coefficients[,2],
+             delta = fit$coefficients[,3],
+             psi   = log(fit$dispersion))
+}, .progress='text')
 
 
