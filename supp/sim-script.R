3f399b8cec83dcbe6c5c0213f89b0af5ba326ae6
Jarad Niemi <niemi@iastate.edu>
2015-07-10 16:51:07 -0500

    use command line args

--- supp/sim-script.R
+++ supp/sim-script.R
@@ -10,44 +10,57 @@ source("single_gene_analysis.R")
 # Compile stan model
 model = stan_model("model.stan")
 
-rds_files = system("ls -S -r ~/temp/rna-seq-stan/simulations/sim1/datasets/*.rds", TRUE)
-results = list()
-for (rds in rds_files) {
-  
-  d = as.data.frame(readRDS(rds)); n = ncol(d)/3
-  names(d) = c(paste("B73_",1:n, sep=''), paste('Mo17_',1:n, sep=''), paste('B73xMo17_',1:n, sep=''))
-  variety = factor(gsub("_[0-9]{1,2}", "", names(d)), levels=c("B73","Mo17","B73xMo17"))
-  
-  # trim genes for average count greater than 1
-  # d = d[which(rowMeans(d)>1),] 
-  hyperparameters = get_hyperparameters(d, variety)
+# Use command line arguments
+# R CMD BATCH --vanilla '--args i=1' sim-script.R 
+args=(commandArgs(TRUE))
 
-  
-  
-  ######################################
-  # Run individual gene analyses
-  ######################################
-  
-  # This will take a long time, so parallelize if possible
-  if (parallel <- require(doMC)) {
-    registerDoMC()
-  } else if (parallel <- require(doParallel)) {
-    cl = makeCluster(detectCores(), type='PSOCK')
-    registerDoParallel(cl)
+if(length(args)==0){
+  print("No arguments supplied.")
+  ##supply default values
+  i = 1
+}else{
+  for(i in 1:length(args)){
+    eval(parse(text=args[[i]]))
   }
-  
-  analysis = adply(d,
-                   1,
-                   function(x) single_gene_analysis(x),
-                   .id = 'gene', 
-                   .parallel = parallel,
-                   .paropts = list(.export=c('single_gene_analysis','model','hyperparameters'), .packages='rstan'))
-  
-  rownames(analysis) = rownames(d)
-  
-
-  results[[rds]] = analysis[,c("prob_LPH","prob_HPH","effectiveSize")]
-  saveRDS(results, file='sim-results.RDS')
 }
+
+rds = system("ls -S -r sim-data/*.rds", TRUE)[i]
+
+d = as.data.frame(readRDS(rds)); n = ncol(d)/3
+names(d) = c(paste("B73_",1:n, sep=''), paste('Mo17_',1:n, sep=''), paste('B73xMo17_',1:n, sep=''))
+variety = factor(gsub("_[0-9]{1,2}", "", names(d)), levels=c("B73","Mo17","B73xMo17"))
+
+# trim genes for average count greater than 1
+# d = d[which(rowMeans(d)>1),] 
+hyperparameters = get_hyperparameters(d, variety)
+
+
+
+######################################
+# Run individual gene analyses
+######################################
+
+# This will take a long time, so parallelize if possible
+if (parallel <- require(doMC)) {
+  registerDoMC()
+} else if (parallel <- require(doParallel)) {
+  cl = makeCluster(detectCores(), type='PSOCK')
+  registerDoParallel(cl)
+}
+
+wh = 1:2
+analysis = adply(d[wh,],
+                 1,
+                 function(x) single_gene_analysis(x),
+                 .id = 'gene', 
+                 .parallel = parallel,
+                 .paropts = list(.export=c('single_gene_analysis','model','hyperparameters'), .packages='rstan'))
+
+rownames(analysis) = rownames(d)[wh]
+
+
+results = analysis[,c("prob_LPH","prob_HPH","effectiveSize")]
+saveRDS(results, file=paste('sim-results',i,'.rds',sep=''))
+
 
 q(ifelse(interactive(), "ask","no"))
