f94468eb43a1e0b6a393cad597f4f73689752b8e
Jarad Niemi <niemi@iastate.edu>
2015-08-11 09:57:06 -0500

    added catf function; added normal sub targets

--- supp/make_Makefile.R
+++ supp/make_Makefile.R
@@ -7,44 +7,46 @@ r_command = 'R CMD BATCH --vanilla'
 
 screen_width = 60
 
-cat(rep('#', screen_width), '\n', sep='', file=makefile, append=FALSE)
-cat('#     DO NOT TOUCH \n', file=makefile, append=TRUE)
-cat(rep('#', screen_width), '\n', sep='', file=makefile, append=TRUE)
-cat('# Automatically created using make_Makefile.R\n\n', 
-    file=makefile, 
-    append=TRUE)
+catf = function(...) cat(..., sep='', file=makefile, append=TRUE)
 
-cat('files = README.txt script.R figs.R get_hyperparameters.R single_gene_analysis.R model.stan\n\nzip: $(files); zip supp.zip $(files)
-', file=makefile, append=TRUE)
+cat(rep('#', screen_width), '\n', file=makefile, append=FALSE)
+catf('#     DO NOT TOUCH \n')
+catf(rep('#', screen_width), '\n')
+catf('# Automatically created using make_Makefile.R\n\n')
 
+# Main targets
+methods = c('laplace')
+
+catf('all: ', paste(methods, collapse=' '), '\n\n')
+
+catf('laplace: ', paste('laplace',n_reps, sep='', collapse=' '), '\n\n')
+
+catf('files = README.txt script.R figs.R get_hyperparameters.R single_gene_analysis.R model.stan\n\nzip: $(files); zip supp.zip $(files)\n\n')
+
+
+# Sub targets
+
+for (m in methods) {
 for (r in n_reps) {
-  cat(rep("#",screen_width), "\n", sep='', file=makefile, append=TRUE)
-  cat("# Targets for ", r, "reps per variety\n", file=makefile, append=TRUE)
-  cat(rep("#",screen_width), "\n", sep='', file=makefile, append=TRUE)  
+  catf(rep('#',screen_width), '\n')
+  catf('# Targets for ', r, 'reps per variety\n')
+  catf(rep('#',screen_width), '\n')  
   
   # Vectors of files
-  result_file = paste0("results/results-", r, "-", 1:n_sims,'.rds')
-  data_file   = paste0("sim-data/sim-", r, "-", 1:n_sims,'.rds')
-  Rout_file   = paste0("sim-", r, "-", 1:n_sims,'.Rout')
+  result_file = paste0('results/',m,'/results-', r, '-', 1:n_sims,'.rds')
+  data_file   = paste0('sim-data/sim-',          r, '-', 1:n_sims,'.rds')
+  Rout_file   = paste0('Rout/',m,'/sim-',        r, '-', 1:n_sims,'.Rout')
   
-  cat("make",r, ': ', paste(result_file, collapse=' '),"\n\n" , 
-      sep = '', 
-    file=makefile, 
-    append=TRUE)
+  catf(m,r, ': ', paste(result_file, collapse=' '),'\n\n')
   
   for (i in 1:n_sims) {
-    cat(result_file[i], ": sim-script.R ", data_file[i], "\n\t", 
+    catf(result_file[i], ": sim-script.R ", data_file[i], "\n\t", 
         r_command, " '--args r=", r, " i=", i, "' sim-script.R ",
-        Rout_file[i], " \n\n",
-        sep='',
-        file=makefile,
-        append=TRUE)
+        Rout_file[i], " \n\n")
   }
   
-  cat("clean",r,":\n\trm -fv ", 
-      paste(result_file, collapse=' '),"\n\n",
-      sep='',
-    file=makefile, 
-    append=TRUE)
+  catf("clean",r,":\n\trm -fv ", 
+      paste(result_file, collapse=' '),"\n\n")
+}
 }
 
