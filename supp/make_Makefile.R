1014b4bca2059b87ea35bbd5c3b6bb3f8933d465
Jarad Niemi <niemi@iastate.edu>
2015-08-11 09:58:25 -0500

    indented

--- supp/make_Makefile.R
+++ supp/make_Makefile.R
@@ -15,7 +15,7 @@ catf(rep('#', screen_width), '\n')
 catf('# Automatically created using make_Makefile.R\n\n')
 
 # Main targets
-methods = c('laplace')
+methods = c('laplace','normal')
 
 catf('all: ', paste(methods, collapse=' '), '\n\n')
 
@@ -27,26 +27,26 @@ catf('files = README.txt script.R figs.R get_hyperparameters.R single_gene_analy
 # Sub targets
 
 for (m in methods) {
-for (r in n_reps) {
-  catf(rep('#',screen_width), '\n')
-  catf('# Targets for ', r, 'reps per variety\n')
-  catf(rep('#',screen_width), '\n')  
-  
-  # Vectors of files
-  result_file = paste0('results/',m,'/results-', r, '-', 1:n_sims,'.rds')
-  data_file   = paste0('sim-data/sim-',          r, '-', 1:n_sims,'.rds')
-  Rout_file   = paste0('Rout/',m,'/sim-',        r, '-', 1:n_sims,'.Rout')
-  
-  catf(m,r, ': ', paste(result_file, collapse=' '),'\n\n')
-  
-  for (i in 1:n_sims) {
-    catf(result_file[i], ": sim-script.R ", data_file[i], "\n\t", 
-        r_command, " '--args r=", r, " i=", i, "' sim-script.R ",
-        Rout_file[i], " \n\n")
+  for (r in n_reps) {
+    catf(rep('#',screen_width), '\n')
+    catf('# Targets for ', r, 'reps per variety\n')
+    catf(rep('#',screen_width), '\n')  
+    
+    # Vectors of files
+    result_file = paste0('results/',m,'/results-', r, '-', 1:n_sims,'.rds')
+    data_file   = paste0('sim-data/sim-',          r, '-', 1:n_sims,'.rds')
+    Rout_file   = paste0('Rout/',m,'/sim-',        r, '-', 1:n_sims,'.Rout')
+    
+    catf(m,r, ': ', paste(result_file, collapse=' '),'\n\n')
+    
+    for (i in 1:n_sims) {
+      catf(result_file[i], ": sim-script.R ", data_file[i], "\n\t", 
+           r_command, " '--args r=", r, " i=", i, "' sim-script.R ",
+           Rout_file[i], " \n\n")
+    }
+    
+    catf("clean",r,":\n\trm -fv ", 
+         paste(result_file, collapse=' '),"\n\n")
   }
-  
-  catf("clean",r,":\n\trm -fv ", 
-      paste(result_file, collapse=' '),"\n\n")
-}
 }
 
