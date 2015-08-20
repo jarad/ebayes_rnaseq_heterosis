2b096f9453e6f53d42744951cf789e04d3c8df97
Jarad Niemi <niemi@iastate.edu>
2015-08-20 15:14:14 -0500

    use args script

--- supp/sim-script.R
+++ supp/sim-script.R
@@ -4,26 +4,7 @@ library(dplyr)
 library(reshape2)
 library(edgeR)
 
-# Need to pass as arguments (or assign)
-# r: number of replicates per variety (4, 8, 16)
-# i: simulation number (1-10)
-# m: statistical method ('laplace' or 'normal')
-
-# R CMD BATCH --vanilla '--args r=4 i=1 m ="laplace"' sim-script.R 
-args=(commandArgs(TRUE))
-
-if(length(args)==0){
-  print("No arguments supplied.")
-  ##supply default values
-  i = 1
-}else{
-  for(ii in 1:length(args)){
-    eval(parse(text=args[[ii]]))
-  }
-}
-
-
-
+source("args.R")
 
 source("get_hyperparameters.R")
 source("single_gene_analysis.R")
