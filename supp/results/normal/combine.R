14920e9f081c6116f7c6dfa3222f87e451126ac0
Jarad Niemi <niemi@iastate.edu>
2015-08-20 16:06:20 -0500

    combined normal results

---
+++ supp/results/normal/combine.R
@@ -0,0 +1,16 @@
+
+
+library(plyr)
+
+combined = ddply(expand.grid(r = c(4,8,16), i = 1:10), .(r,i), function(x) {  
+  d = readRDS(paste("sim-",x$r,"-",x$i,".rds", sep=''))
+  data.frame(geneid = rownames(d),
+             sim = x$i, 
+             sample.size = x$r, 
+             phph = d$prob_HPH, 
+             plph = d$prob_LPH)
+})
+
+combined$r = combined$i = NULL
+
+saveRDS(combined, file="eBayes_normal_stan.rds")
