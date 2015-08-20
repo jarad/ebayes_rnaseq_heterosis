bac0cc98bb36e30fdb5c0373e2c6f1e8cc4a8d2a
Jarad Niemi <niemi@iastate.edu>
2015-08-20 15:13:51 -0500

    can now be used to analyze sims

--- supp/ji-sim-script.R
+++ supp/ji-sim-script.R
@@ -2,23 +2,29 @@ library(plyr)
 library(dplyr)
 library(reshape2)
 
+source("args.R")
+
 source("empirical_estimate_function.R")
 
 # Data set
-original = read.csv("data.csv")[1:2640,]
+data_file = paste0("sim-data/sim-", r, "-", i,'.rds')
 
-d = original %>%
-  rename(geneID = X) %>%
+d = as.data.frame(readRDS(data_file)); n = ncol(d)/3
+names(d) = c(paste("B73_",1:n, sep=''), paste('Mo17_',1:n, sep=''), paste('B73xMo17_',1:n, sep=''))
+variety = factor(gsub("_[0-9]{1,2}", "", names(d)), levels=c("B73","Mo17","B73xMo17"))
+d$geneID = 1:nrow(d)
+
+ld = d %>%
   melt(id.vars="geneID", variable.name="sampleID", value.name="count") %>%
   mutate(variety = factor(gsub("_[0-9]{1,2}", "", sampleID), levels=c("B73","Mo17","B73xMo17")),
          y = log(count+1)) 
   
 # Summary statistics
-tmp = d %>% 
+tmp = ld %>% 
   group_by(geneID, variety) %>%
   summarize(n = length(y), mean = mean(y), SS=sum((y-mean)^2)) 
 
-# Check to make sure # reps per variety are all the same and assign to I
+# Check to make sure # reps per variety are all the same 
 stopifnot(abs(max(tmp$n) - min(tmp$n)) < .5)
 
 ss = ddply(tmp, .(geneID), function(x) {
@@ -34,5 +40,5 @@ est = estimate(simd = NULL,              # Was used for ROC calculations
                delta.obs = ss$delta_hat, 
                s2.ej.obs = ss$S2, 
                I = tmp$n[1],             # number of reps per variety
-               J = nrow(original),       # number of genes 
+               J = nrow(d),              # number of genes 
                mcn = 1000)               # Monte Carlo reps?
