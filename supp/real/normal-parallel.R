44fcfd356b0b860a1db4d268bbfdd494504b8cdb
Jarad Niemi <niemi@iastate.edu>
2015-08-24 16:30:27 -0500

    distinct script

---
+++ supp/real/normal-parallel.R
@@ -0,0 +1,48 @@
+library(rstan)
+library(plyr)
+library(dplyr)
+library(reshape2)
+library(edgeR)
+
+source("../common/get_hyperparameters.R")
+source("../common/single_gene_analysis.R")
+
+##################################################
+# Get hyperparameters
+##################################################
+d = read.csv("data/data.csv", row.names=1)
+variety = factor(gsub("_[0-9]{1,2}", "", names(d)), levels=c("B73","Mo17","B73xMo17"))
+
+# trim genes for average count greater than 1
+#d = d[which(rowMeans(d)>1),] 
+hyperparameters = get_hyperparameters(d, variety, method="normal")
+
+
+
+##################################################
+# Perform individual gene analsysis (in parallel)
+##################################################
+
+# Compile stan model
+model = stan_model("../common/normal.stan")
+
+# This will take a long time, so parallelize if possible
+require(doMC)
+registerDoMC(5)
+
+sink("script.temp")
+
+analysis = adply(d,
+                 1,
+                 function(x) single_gene_analysis(x),
+                 .id = 'gene',
+                 .parallel = TRUE,
+                 .paropts = list(.export=c('single_gene_analysis','model','hyperparameters'), .packages='rstan'))
+
+sink()
+unlink("script.temp")
+
+rownames(analysis) = rownames(d)
+saveRDS(analysis, file="results/normal-parallel.rds")
+
+q(ifelse(interactive(), "ask", "no"))
