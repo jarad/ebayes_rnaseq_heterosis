6767158326f27b5a2312b2a59c061c1260e31169
Jarad Niemi <niemi@iastate.edu>
2015-08-25 10:04:33 -0500

    combine eBayes approaches into single file

---
+++ supp/real/eBayes.R
@@ -0,0 +1,52 @@
+library(rstan)
+library(plyr)
+library(dplyr)
+library(reshape2)
+library(edgeR)
+
+source("../common/get_hyperparameters.R")
+source("../common/single_gene_analysis.R")
+
+if (parallel) source("parallel.R")
+
+##################################################
+# Get hyperparameters
+##################################################
+d = read.csv("data/data.csv", row.names=1)
+variety = factor(gsub("_[0-9]{1,2}", "", names(d)), levels=c("B73","Mo17","B73xMo17"))
+
+# trim genes for average count greater than 1
+#d = d[which(rowMeans(d)>1),] 
+hyperparameters = get_hyperparameters(d, variety, method=method)
+
+
+
+##################################################
+# Perform individual gene analsysis (in parallel)
+##################################################
+
+# Compile stan model
+model = stan_model(paste0("../common/",method,".stan"))
+
+con = 'script.temp'
+sink(con)
+sink(con, type='message')
+
+analysis = adply(d,
+                 1,
+                 function(x) single_gene_analysis(x),
+                 .id = 'gene',
+                 .parallel = parallel,
+                 .paropts = list(.export=c('single_gene_analysis','model','hyperparameters'), .packages='rstan'))
+
+sink()
+sink(type='message')
+unlink(con)
+
+rownames(analysis) = rownames(d)
+saveRDS(analysis, file=paste0("results/",
+                              method,
+                              ifelse(parallel, '-parallel', NULL),
+                              ".rds"))
+
+q(ifelse(interactive(), "ask", "no"))
