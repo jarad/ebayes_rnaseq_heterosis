2f7d8cd3a7a0696f6ba88c1c9f3cde5cefd64730
Jarad Niemi <niemi@iastate.edu>
2015-08-25 15:22:31 -0500

    add Will's files

---
+++ supp/real/real_data_pvals.R
@@ -0,0 +1,22 @@
+library(edgeR)
+library(baySeq)
+library(ShrinkBayes)
+
+source("andyspost.R")
+source("utils.R")
+
+real_data_pvals = function(ncores = 1){
+  group = as.factor(rep(c("parent1", "parent2", "hybrid"), each = 4))
+  cts = as.matrix(readRDS("paschold_counts.rds"))
+  cts = trim_genes(cts, group, rownames(cts))[[1]]
+  for(mtd in c("edgeR", "baySeq", "ShrinkBayes")){
+    for(cp in ncores){
+      print(mtd)
+      print(cp)
+      t = proc.time()
+      r = ranks1dataset(mtd, 4, 1, cts, group, ncpus = ncores)
+      saveRDS(proc.time() - t, paste0("time_", mtd, "_", cp, "_cores", ".rds"))
+      saveRDS(r, paste0("pvals_", mtd, "_", cp, "_cores", ".rds"))
+    }
+  }
+}
